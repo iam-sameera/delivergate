4ffd9904b683e0ffc93ff8bd4220d4ae317e78cc
Sameera Dissanayaka <161738305+iam-sameera@users.noreply.github.com>
2024-05-20 20:45:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello, Flask is running in a Docker container!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
